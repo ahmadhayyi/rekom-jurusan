8235d964573b88b0153b441448bdf75eee5310be
ahmadhayyi <ahmadhayyi33@gmail.com>
2023-01-17 13:05:05 +0700

    Menambah total siswa pada jurusan

--- resources/views/dashboard/jurusan/index.blade.php
+++ resources/views/dashboard/jurusan/index.blade.php
@@ -15,6 +15,7 @@
                     class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                     <th class="px-4 py-3">No</th>
                     <th class="px-4 py-3">Nama</th>
+                    <th class="px-4 py-3">Total Siswa</th>
                     <th class="px-4 py-3">Aksi</th>
                 </tr>
             </thead>
@@ -23,6 +24,7 @@
                 <tr class="text-gray-700 dark:text-gray-400">
                     <td class="px-4 py-3 w-[10px]">{{ $loop->iteration }}</td>
                     <td class="px-4 py-3">{{ $item->nama }}</td>
+                    <td class="px-4 py-3">{{ $item->kmeans->count() }}</td>
                     <td class="px-4 py-3">
                         <div class="flex items-center space-x-4 text-sm">
                             @include('dashboard.components.edit')
